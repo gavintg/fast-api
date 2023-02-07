8887f59aeb7ed7c9e7eec91052f67310ade01d4c
Katya Ivanova <katyaivanova0799@gmail.com>
2023-02-07 23:58:45 +0000

    Test the secure purchase end point

--- tests/e2e/test_purchase_route.py
+++ tests/e2e/test_purchase_route.py
@@ -4,4 +4,14 @@ import requests
 class TestPurchase:
 
     def test_get_purchases(self):
-        assert False
+        result = requests.post('http://localhost:8000/token', json={
+            'grant_type': 'password', 
+            'scope': 'read_write', 
+            'client_id': 'ValidTokenUser', 
+            'client_secret': 'ValidTokenPassword'})
+        data = json.loads(result.content)
+        
+        response = requests.get('http://localhost:8000/purchase', 
+                headers={"token" : data['access_token']})
+        assert response.status_code == 200
+        assert response.content != None
