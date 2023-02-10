dda75afa6c5a9322799af0d00fc44eef79077dad
Katya Ivanova <katyaivanova0799@gmail.com>
2023-02-10 23:07:28 +0000

    Find a bug in the token end point

--- tests/e2e/test_purchase_route.py
+++ tests/e2e/test_purchase_route.py
@@ -1,14 +1,28 @@
 import json
 import requests
-from api.routes.token_repository import TokenRepository
 
 class TestPurchase:
 
-    token_repository = TokenRepository()
+    def get_valid_user(self):
+        requests.post('http://localhost:8000/user', json={'username': 'tokentest', 'password': 'mytokenpassword'})
+
+    def get_access_token(self):
+        result = requests.post('http://localhost:8000/token', json={
+            'grant_type': 'password', 
+            'scope': 'read_write', 
+            'client_id': 'tokentest', 
+            'client_secret': 'mytokenpassword'})
+        assert result.status_code == 200
+        data = json.loads(result.content)
+        print("Firsttttt",data)
+        return data['access_token']
 
     def test_get_purchases(self):
-        token = self.token_repository.insert_token()
+        self.get_valid_user()
+        data = self.get_access_token()
+        print("Thissss", data)
         
-        response = requests.get('http://localhost:8000/purchase', headers={"token" : token})
+        response = requests.get('http://localhost:8000/purchase', 
+                headers={"token" : data})
         assert response.status_code == 200
         assert response.content != None
